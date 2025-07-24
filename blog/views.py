fbdfac0ee919dfe3cce503a6fafa4a9ac3560eca
Manveer101 <Manveer.singh@codearrest.com>
2025-07-24 23:20:32 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -14,15 +14,15 @@ class BlogViewSet(ModelViewSet):
     # Enable filters, ordering, and search
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
 
-    # Filter by these fields in URL: ?author=1 or ?title=Some
+    # Filter by these fields: ?author=1 or ?title=Some
     filterset_fields = ['author', 'title']
 
-    # Search by keyword: ?search=something
+    # Search by: ?search=something
     search_fields = ['title', 'content']
 
     # Order results by: ?ordering=created_at or ?ordering=title
     ordering_fields = ['created_at', 'title']
 
-    # Assign the logged-in user as author
+    # Make the logged-in user as author
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
